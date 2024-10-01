b01f2f4f48f48cccd3e498ccba81906914388ce7
simone-corsini <simone.corsini@live.com>
2024-10-01 17:27:08 +0200

    .

--- process_data.py
+++ process_data.py
@@ -292,7 +292,7 @@ if __name__ == '__main__':
 
     progress.console.print('[green]Caricamento dati[/green]')
 
-    df = pd.read_csv(args.input, nrows=10000)
+    df = pd.read_csv(args.input)
     os.makedirs(f'{base_path}/processed', exist_ok=True)
     os.makedirs(f'{base_path}/test', exist_ok=True)
     os.makedirs(f'{base_path}/val', exist_ok=True)
