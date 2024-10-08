2d29d3548a5b25cea3bafe7b385b39363b6c3da1
simone-corsini <simone.corsini@live.com>
2024-10-08 17:23:49 +0200

    .

---
+++ prepare_sets.sh
@@ -0,0 +1,4 @@
+python3 process_data.py -ol 30 -fl 5 -t 2 -tt max
+python3 process_data.py -ol 30 -fl 10 -t 2 -tt max
+python3 process_data.py -ol 30 -fl 5 -t 2 -tt mean
+python3 process_data.py -ol 30 -fl 10 -t 2 -tt mean
